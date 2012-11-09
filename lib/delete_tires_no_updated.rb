5a61b07c952595deedc0b785945e0982663536be
jose miguel <miguel.gamazo@galiclick.com>
2012-11-09 19:12:51 +0100

    Mas mejoras

--- lib/delete_tires_no_updated.rb
+++ lib/delete_tires_no_updated.rb
@@ -2,6 +2,8 @@ class DeleteTiresNoUpdated
   def initialize()
     @delete_in_gane = 0
     @delete_in_eurotyre = 0
+    @total_eurotyre = 0
+    @total_gane = 0
     I18n.locale = 'es'
   end
 
@@ -11,13 +13,17 @@ class DeleteTiresNoUpdated
   end
 
   def delete_tires
-    @delete_in_eurotyre = Spree::Product.where("updated_at < ? and supplier_id = ?", Date.yesterday, 2027).destroy_all
-    @delete_in_gane = Spree::Product.where("updated_at < ? and supplier_id = ?", Date.yesterday, 1045).destroy_all
+    @delete_in_eurotyre = Spree::Product.where("updated_at < ? and supplier_id = ?", Date.yesterday, 2027)
+    @total_eurotyre = @delete_in_eurotyre.count
+    @delete_in_eurotyre.destroy_all
+    @delete_in_gane = Spree::Product.where("updated_at < ? and supplier_id = ?", Date.yesterday, 1045)
+    @total_gane = @delete_in_gane.count
+    @delete_in_gane.destroy_all
   end
 
   def send_mail
     begin
-      Spree::NotifyMailer.report_deleted_tires(@delete_in_gane.count, @delete_in_eurotyre.count).deliver
+      Spree::NotifyMailer.report_deleted_tires(@total_gane, @total_eurotyre).deliver
     rescue Exception => e
       logger.error("#{e.class.name}: #{e.message}")
       logger.error(e.backtrace * "\n")
